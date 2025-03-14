e2c4d4008c4267525df52203c314f7455ac405ec
ayaan <zxcvbnm5288@gmail.com>
2025-03-14 23:19:11 +0900

    feat: #14.2

--- tiktok_clone/lib/features/videos/widgets/video_post.dart
+++ tiktok_clone/lib/features/videos/widgets/video_post.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
@@ -47,6 +48,12 @@ class _VideoPostState extends State<VideoPost>
   void _initializeVideoPlayer() async {
     await _videoPlayerController.initialize();
     await _videoPlayerController.setLooping(true);
+
+    // NOTE: The following code is a conditional statement that checks if the platform is web.
+    if (kIsWeb) {
+      await _videoPlayerController.setVolume(0);
+    }
+
     _videoPlayerController.addListener(_onVideoChange);
     setState(() {});
   }
