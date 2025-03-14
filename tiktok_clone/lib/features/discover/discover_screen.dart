7ffcfd7f4d2afd5ac4b4db8105ced9ee047b2e99
ayaan <zxcvbnm5288@gmail.com>
2025-03-14 23:27:03 +0900

    feat: #14.3

--- tiktok_clone/lib/features/discover/discover_screen.dart
+++ tiktok_clone/lib/features/discover/discover_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/breakpoints.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
@@ -34,6 +35,8 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Size(:width) = MediaQuery.of(context).size;
+
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
@@ -63,8 +66,8 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
             GridView.builder(
               keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
               padding: const EdgeInsets.all(Sizes.size6),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: width > Breakpoints.md ? 5 : 2,
                 childAspectRatio: 9 / 21,
                 mainAxisSpacing: Sizes.size10,
                 crossAxisSpacing: Sizes.size10,
